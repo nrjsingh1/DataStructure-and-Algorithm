95f3766838826b2b2f8b3397a15b960b7d2376a0
nrjsingh1 <nrjsingh1@yahoo.in>
2019-05-18 15:55:06 +0530

    Salary problem codechef

---
+++ src/codechefDSA/Salary.java
@@ -0,0 +1,12 @@
+package codechefDSA;
+
+/*
+ * The Minimum Number Of Moves, Problem Code: SALARY
+ * */
+public class Salary {
+
+	public static void main(String args[]) {
+		System.out.println("Hello Hello");
+	}
+	
+}
